e03fe38439f7996fecdaab6c02a33265793c01a5
Angel-4-1 <angel.vicente01@estudiant.upf.edu>
2020-05-19 16:54:22 +0200

    Cameras included

---
+++ Assets/Scripts/Elevator.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Elevator : MonoBehaviour
+{
+    public float max;
+    public int up;
+    public float speed;
+
+    void Start()
+    {
+        up = 1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 moveDirection;
+
+        if ((transform.position.y < max && up == 1) || transform.position.y <= 0)
+        {
+            up = 1;
+            moveDirection = Vector3.up;
+        }
+        else
+        {
+            up = 0;
+            moveDirection = Vector3.down;
+        }
+
+        transform.Translate(moveDirection * Time.deltaTime * speed);
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+    public Text countText;
+    public Text winText;
+    public AudioSource pick;
+    public AudioSource badPick;
+
+    private Rigidbody rb;
+    private int count;
+    private int goodPickUps;
+    private int collected;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        count = 0;
+        goodPickUps = 13;
+        collected = goodPickUps;
+        SetCountText();
+        winText.text = "";
+    }
+    
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+
+        rb.AddForce(movement * speed);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Pick Up"))
+        {
+            other.gameObject.SetActive(false);
+            count++;
+            collected--;
+            pick.Play();
+            SetCountText();
+        }
+
+        if (other.gameObject.CompareTag("Bad Pick Up"))
+        {
+            other.gameObject.SetActive(false);
+            count--;
+            badPick.Play();
+            SetCountText();
+        }
+    }
+
+    void SetCountText()
+    {
+        countText.text = "Count: " + count.ToString();
+        if (collected == 0)
+        {
+            if (count == goodPickUps)
+            {
+                winText.text = "You Win!";
+            }
+            else
+            {
+                winText.text = "Game over!";
+            }
+        }
+    }
+}
